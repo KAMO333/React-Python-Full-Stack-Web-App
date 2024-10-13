9ef189e7df258b7d2b819fefaea0e07831c3b8b9
Kamogelo Mmopane <101883751+KAMO333@users.noreply.github.com>
2024-10-13 17:20:44 +0200

    python basics

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,11 @@
-print('Hello World')
+def my_func(name):
+    if name == 'John':
+        print('John has enter the building')
+    else:
+        print('it is not john but it is:', name)
+
+    for i in range(5):
+        print(i, 'seconds have passed')
+
+my_func('Jane')
+
