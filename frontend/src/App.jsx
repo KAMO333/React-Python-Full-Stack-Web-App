8b847553e293e436a86a091dca4627330a08870c
@kammopajhb024 <mmopanemoroka@gmail.com>
2024-11-28 15:56:29 +0200

    home text

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,33 @@
-import { Container, Stack } from "@chakra-ui/react";
+import { Container, Stack, Text } from "@chakra-ui/react";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <>
-      <Stack minH={"100vh"}>
-        <Navbar />
-        <Container maxH={"1200px"} my={4}></Container>
-      </Stack>
-    </>
+    <Stack minH={"100vh"}>
+      <Navbar />
+
+      <Container maxW={"1200px"} my={4}>
+        <Text
+          fontSize={{ base: "3xl", md: "50" }}
+          fontWeight={"bold"}
+          letterSpacing={"2px"}
+          textTransform={"uppercase"}
+          textAlign={"center"}
+          mb={8}
+        >
+          <Text
+            as={"span"}
+            bgGradient="to-r"
+            gradientFrom="cyan.400"
+            gradientTo="blue.500"
+            bgClip={"text"}
+          >
+            My Besties
+          </Text>
+          🚀
+        </Text>
+      </Container>
+    </Stack>
   );
 }
 
